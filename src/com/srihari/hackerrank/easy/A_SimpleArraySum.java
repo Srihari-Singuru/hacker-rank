a49bf97211f085dd9c32223f75ac61ff438e0445
Srihari Singuru <srihari.singuru@outlook.com>
2020-06-01 08:07:26 +0530

    Sum of elements of an integer array

--- src/com/srihari/hackerrank/easy/A_SimpleArraySum.java
+++ src/com/srihari/hackerrank/easy/A_SimpleArraySum.java
@@ -23,24 +23,8 @@ public class A_SimpleArraySum {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) throws IOException {
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
-
-        int arCount = Integer.parseInt(scanner.nextLine().trim());
-
-        int[] ar = new int[arCount];
-
-        String[] arItems = scanner.nextLine().split(" ");
-
-        for (int arItr = 0; arItr < arCount; arItr++) {
-            int arItem = Integer.parseInt(arItems[arItr].trim());
-            ar[arItr] = arItem;
-        }
-
-        int result = simpleArraySum(ar);
-
-        bufferedWriter.write(String.valueOf(result));
-        bufferedWriter.newLine();
-
-        bufferedWriter.close();
+        int[] arr = {4, 5, 6};
+        int result = simpleArraySum(arr);
+        System.out.println(result);
     }
 }
