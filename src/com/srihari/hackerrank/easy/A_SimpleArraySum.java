6514e596f6fbb353df2165fe5427496e336c0e95
Srihari Singuru <srihari.singuru@outlook.com>
2020-06-01 08:08:33 +0530

    Sum of elements of an integer array

--- src/com/srihari/hackerrank/easy/A_SimpleArraySum.java
+++ src/com/srihari/hackerrank/easy/A_SimpleArraySum.java
@@ -1,7 +1,5 @@
 package com.srihari.hackerrank.easy;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Scanner;
@@ -22,7 +20,7 @@ public class A_SimpleArraySum {
 
     private static final Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         int[] arr = {4, 5, 6};
         int result = simpleArraySum(arr);
         System.out.println(result);
