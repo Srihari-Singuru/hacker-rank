af032472515dd2b045f06349bca2b034a985545e
Srihari Singuru <srihari.singuru@outlook.com>
2020-06-05 23:17:39 +0530

    Tree - medium

---
+++ src/com/srihari/hacker_rank/data_structures/f_trees/b_medium/A_ValidBinarySearchTree.java
@@ -0,0 +1,30 @@
+package com.srihari.hacker_rank.data_structures.f_trees.b_medium;
+
+/**
+ * https://www.hackerrank.com/challenges/is-binary-search-tree/problem
+ */
+public class A_ValidBinarySearchTree {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+    }
+
+    static boolean checkBST(Node root) {
+        return checkBST(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+
+    private static boolean checkBST(Node root, int min, int max){
+        if(root == null){
+            return true;
+        }
+        if(root.data <= min || root.data >= max){
+            return false;
+        }
+        return checkBST(root.left, min, root.data) && checkBST(root.right, root.data, max);
+    }
+
+    public static void main(String[] args) {
+        checkBST(null); // we can pass a tree here
+    }
+}
