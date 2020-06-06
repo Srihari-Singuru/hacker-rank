48cef02b10efca76b1ecbbf742045ed2830b23e2
Srihari Singuru <srihari.singuru@outlook.com>
2020-06-06 17:32:09 +0530

    DailyCodingChallenge - Easy

---
+++ src/com/srihari/daily_coding/a_easy/A_TwoSum.java
@@ -0,0 +1,31 @@
+package com.srihari.daily_coding.a_easy;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+ * For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+ *
+ * Bonus: Can you do this in one pass?
+ */
+public class A_TwoSum {
+    public static void main(String[] args) {
+        int[] arr = {10, 15, 3, 7};
+        int k = 17;
+
+        boolean isPresent = twoSum(arr, k);
+        System.out.println(isPresent);
+    }
+
+    private static boolean twoSum(int[] arr, int k) {
+        Set<Integer> set = new HashSet<>();
+        for(int elem : arr){
+            if(set.contains(k - elem)){
+                return true;
+            }
+            set.add(elem);
+        }
+        return false;
+    }
+}
